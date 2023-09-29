22c0367cede50772839c74ce68d49bc62eaade14
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-09-29 12:05:15 +0300

    initial commit

---
+++ Back-End/ChocolateFactory/src/main/java/com/example/chocolatefactory/ChocolateFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.chocolatefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolateFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolateFactoryApplication.class, args);
+	}
+
+}
---
+++ Back-End/ChocolateFactory/src/test/java/com/example/chocolatefactory/ChocolateFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chocolatefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
