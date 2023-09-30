56f92356137c570e487d7af09d267ad7bd2ec78c
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-09-30 19:52:26 +0300

    add

--- Front-End/chocolate-factory/src/app/app-routing.module.ts
+++ Front-End/chocolate-factory/src/app/app-routing.module.ts
@@ -1,99 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home/home.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'login',
-    // component: LoginComponent,
-  },
-  {
-    path: 'register',
-    // component: RegisterComponent,
-  },
-  {
-    path: 'about',
-    // component: AboutComponent,
-    pathMatch: 'full',
-  },
-  {
-    path: 'locations',
-    // component: LocationHomeComponent,
-    pathMatch: 'full',
-  },
-  {
-    path: 'add',
-    // component: ProductCreateComponent,
-    // canActivate: [AuthActivate],
-    data: {
-      authenticationRequired: true,
-      authenticationFailureRedirectUrl: 'home',
-    },
-  },
-  {
-    path: 'confirm',
-    // component: OrderConfirmComponent,
-    // canActivate: [AuthActivate],
-    data: {
-      authenticationRequired: true,
-      authenticationFailureRedirectUrl: 'login',
-    },
-  },
-  {
-    path: 'all',
-    // component: ProductAllComponent,
-    // canActivate: [AuthActivate],
-    data: {
-      authenticationRequired: true,
-      authenticationFailureRedirectUrl: 'home',
-    },
-  },
-  {
-    path: 'reviews',
-    // component: ReviewHomeComponent,
-    // canActivate: [AuthActivate],
-    data: {
-      authenticationRequired: true,
-      authenticationFailureRedirectUrl: 'home',
-    },
-  },
-  {
-    path: 'details/:productId',
-    // component: ProductDetailsComponent,
-    // canActivate: [AuthActivate],
-    data: {
-      authenticationRequired: true,
-      authenticationFailureRedirectUrl: 'home',
-    },
-    pathMatch: 'full',
-  },
-  {
-    path: 'profile',
-    // component: ProfileComponent,
-    // canActivate: [AuthActivate],
-    data: {
-      authenticationRequired: true,
-      authenticationFailureRedirectUrl: 'login',
-    },
-  },
-  {
-    path: '**',
-    // component: NotFoundComponent,
-    pathMatch: 'full',
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- Front-End/chocolate-factory/src/app/app.module.ts
+++ Front-End/chocolate-factory/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonsModule } from './commons/commons.module';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CommonsModule, HomeModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
