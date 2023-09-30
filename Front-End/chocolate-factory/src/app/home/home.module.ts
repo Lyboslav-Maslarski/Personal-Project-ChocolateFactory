47f3e97f9aa82f9208661f64a9e4293af1203ef9
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-09-30 09:23:38 +0300

    add

--- Front-End/chocolate-factory/src/app/home/home.module.ts
+++ Front-End/chocolate-factory/src/app/home/home.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [HomeComponent, AboutComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserModule, FormsModule, NgbModule],
+  bootstrap: [HomeComponent],
+  exports: [HomeComponent],
 })
 export class HomeModule {}
--- Front-End/chocolate-factory/src/app/home/home/home.component.ts
+++ Front-End/chocolate-factory/src/app/home/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig],
 })
 export class HomeComponent {
+  images = [
+    'https://res.cloudinary.com/dmncdcgx9/image/upload/v1696043900/footerBackground_mbyuup.jpg',
+    'https://res.cloudinary.com/dmncdcgx9/image/upload/v1696045070/whiteTopTransparent_kz6jbx.png',
+  ];
 
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 }
