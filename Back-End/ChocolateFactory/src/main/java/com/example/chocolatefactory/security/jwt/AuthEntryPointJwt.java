a57c677282671c08b8d994f1e5e1be1570cbeb31
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-10-02 07:08:52 +0300

    initial commit

--- Back-End/ChocolateFactory/src/main/java/com/example/chocolatefactory/security/jwt/AuthEntryPointJwt.java
+++ Back-End/ChocolateFactory/src/main/java/com/example/chocolatefactory/security/jwt/AuthEntryPointJwt.java
@@ -1,6 +1,5 @@
 package com.example.chocolatefactory.security.jwt;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
