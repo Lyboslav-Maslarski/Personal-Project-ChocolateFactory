3592b02c6921ef0ed9887723274e4eff2f428429
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-10-13 20:38:18 +0300

    add

--- Back-End/ChocolateFactory/src/main/java/com/example/chocolatefactory/domain/requestDTOs/order/OrderAddDTO.java
+++ Back-End/ChocolateFactory/src/main/java/com/example/chocolatefactory/domain/requestDTOs/order/OrderAddDTO.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
-public record OrderAddReqDTO(@NotNull @Size(min = 1) List<Long> products){
+public record OrderAddDTO(@NotNull @Size(min = 1) List<Long> products){
 }
